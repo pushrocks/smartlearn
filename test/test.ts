1111a1a37fccb9ca7547bbfa4d158c7ec9fe63b8
Phil Kunz <gitlab@philkunz.com>
2017-10-07 03:01:03 +0200

    initial

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { expect, tap } from 'tapbundle'
+import * as smartlearn from '../ts/index'
+
+tap.test('first test', async () => {
+  console.log(smartlearn.standardExport)
+})
+
+tap.start()
---
+++ ts/index.ts
@@ -0,0 +1,3 @@
+import * as plugins from './smartlearn.plugins'
+
+export let standardExport = 'Hi there! :) This is a exported string'
